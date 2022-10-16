78c12e995c9769fd02370caf669599f1c5b6f820
Rutuja Padgilwar <rutujapadgilwar@Rutujas-MacBook-Pro.local>
2022-10-16 16:18:42 -0700

    Solutionfor hw2

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -1,7 +1,32 @@
 /** Exercise 04 - API **/
 
-const url = 'https://restcountries.com/v3.1/all';
+const url = "https://restcountries.com/v3.1/all";
+async function displayData() {
+  var response = await fetch(url);
+  console.log(response.ok);
+  if(response.ok === false || response.body === ""){
+    document.getElementById("results").innerHTML = "Oops! It seems that the external url is not working."
+    return;
+  }
+  var data = await response.json();
+  var list = [];
+  for (var i = 0; i < data.length; i++) {
+    list[i] =
+      data[i]["name"]["common"] +
+      "-" +
+      data[i]["population"] +
+      "</li>";
+  }
+  
+  list.sort();
+  
+  var result = '';
+  for(var i in list){
+    result += ("<li>" +
+    list[i] +
+    "</li>")
+  }
+  console.log(list);
+  document.getElementById("results").innerHTML = result;
+}
 
-// Add your code here
-
-getData(url);
